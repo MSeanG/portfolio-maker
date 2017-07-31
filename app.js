0aa71d23755bc5da7513fd0dc586ed6336ee0a25
Sean <sgilmore@michael-sean.com>
2017-07-31 12:10:03 -0400

    Hooked up mongo db in app.js and required dotenv config

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
+var mongoose = require('mongoose');
 
+require('dotenv').config();
+
+var app = express();
+mongoose.connect(process.env.MONGODB_URI);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
